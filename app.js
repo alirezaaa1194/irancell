520b527bbd71165d7f631ef162253f7626995888
Alireza <alirezaaa1194@gmail.com>
2022-09-18 16:34:17 +0430

    fix bug6

--- app.js
+++ app.js
@@ -1,3 +1,36 @@
 function menuBtnFunction(menuBtn) {
     menuBtn.classList.toggle("active");
 }
+let searchlink=document.getElementById('searchlink');
+let txt=document.getElementById('txt');
+let box=document.getElementById('box');
+
+txt.addEventListener("click", function a(){
+box.style.backgroundColor='#fff'
+txt.style.width='100%'
+txt.style.paddingRight='3px'
+txt.style.paddingLeft='3px'
+})
+searchlink.addEventListener("click", function a(){
+    box.style.backgroundColor='#fff'
+    box.style.width='150px'
+    txt.style.width='100%'
+    txt.style.paddingRight='3px'
+    txt.style.paddingLeft='3px'
+    })
+function banone(){
+    box.style.background='none'
+    box.style.width='39px'
+    box.style.borderColor='#fc0'
+}
+box.addEventListener("click", function b(){
+    box.style.width='150px'
+    box.style.borderWidth='2px';
+    box.style.borderStyle='solid';
+    box.style.borderColor='black';
+    txt.style.paddingRight='3px'
+    txt.style.paddingLeft='3px'
+    if(txt.value.length>0){
+        searchlink.setAttribute('href', 'https://google.com');
+    }
+})
